58e2f4fe8c7fe447d548e5069e30ffb7b37c257a
Breno Angelotti <brenogangelotti@gmail.com>
2020-09-21 22:27:19 -0300

    searching list

--- BurgerMonkeys/BurgerMonkeys/ViewModels/MainViewModel.cs
+++ BurgerMonkeys/BurgerMonkeys/ViewModels/MainViewModel.cs
@@ -2,20 +2,34 @@
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using BurgerMonkeys.Model;
 using BurgerMonkeys.Services;
+using BurgerMonkeys.Tools;
 
 namespace BurgerMonkeys.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
         public ObservableCollection<Post> Items { get; set; }
+        public List<Post> AllItems { get; set; }
 
         readonly IPostService _postService;
         readonly IWpService _wpService;
 
-        public ICommand  RefreshCommand { get; }
+        private string _searchText;
+        public string SearchText {
+            get => _searchText;
+            set {
+                SetProperty(ref _searchText, value);
+                SearchAsync();
+            }
+        }
+
+        public ICommand RefreshCommand { get; }
+
+        public ICommand SearchCommand { get; }
 
         public MainViewModel(IPostService postService,
                              IWpService wpService)
@@ -24,30 +38,59 @@ namespace BurgerMonkeys.ViewModels
             _wpService = wpService;
             Items = new ObservableCollection<Post>();
             RefreshCommand = new Command(ExecuteRefreshCommand);
+            SearchCommand = new Command(ExecuteSearchCommand);
         }
 
         private async void ExecuteRefreshCommand()
         {
-            await GetPosts();
+            await GetPostsAsync();
         }
 
         public async override Task InitializeAsync()
         {
             if (Items.Any())
                 return;
-            await GetPosts().ConfigureAwait(false);
+            await GetPostsAsync().ConfigureAwait(false);
         }
 
-        private async Task GetPosts()
+        private async Task GetPostsAsync()
         {
             var wpPosts = await _wpService.GetAll().ConfigureAwait(false);
-            var items = await _postService.Convert(wpPosts).ConfigureAwait(false);
+            AllItems = (await _postService.Convert(wpPosts).ConfigureAwait(false)).ToList();
             Items.Clear();
-            foreach (var item in items)
+            foreach (var item in AllItems)
             {
                 Items.Add(item);
             }
             IsBusy = false;
+        }
+
+        private async void ExecuteSearchCommand()
+        {
+            await SearchAsync();
+        }
+
+        private async Task SearchAsync()
+        {
+            var resultItems = new List<Post>();
+
+            if (SearchText.IsNullOrWhiteSpace())
+            {
+                Items.Clear();
+                AllItems.ForEach(i => Items.Add(i));
+                return;
+            }
+
+            var cleanSearchText = SearchText.IgnoreCaseSensitiveAndAccents();
+
+            resultItems = AllItems.Where(i =>
+                i.Title.IgnoreCaseSensitiveAndAccents()
+                    .Contains(cleanSearchText) ||
+                i.Author.IgnoreCaseSensitiveAndAccents()
+                    .Contains(cleanSearchText)
+                ).ToList();
+            Items.Clear();
+            resultItems.ForEach(i => Items.Add(i));
         }
     }
 }
