759633be79686939bfaee0a33e06d05c42b93858
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-11-23 22:03:30 -0300

    Criando tela de perfil do autor

---
+++ BurgerMonkeys/BurgerMonkeys/ViewModels/AuthorProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BurgerMonkeys.Model;
+
+namespace BurgerMonkeys.ViewModels
+{
+    public class AuthorProfileViewModel : BaseViewModel
+    {
+        public AuthorProfileViewModel(Author author)
+        {
+        }
+    }
+}
