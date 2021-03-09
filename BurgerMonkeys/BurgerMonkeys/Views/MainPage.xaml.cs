6e81abaa2a84b92f31ac4641a230bf93d9100934
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-08 22:26:30 -0300

    Implementar inicialização da view model ao trocar aba selecionada

--- BurgerMonkeys/BurgerMonkeys/Views/MainPage.xaml.cs
+++ BurgerMonkeys/BurgerMonkeys/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using BurgerMonkeys.Abstractions;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
 
 namespace BurgerMonkeys
 {
@@ -7,6 +9,17 @@ namespace BurgerMonkeys
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void TabView_SelectionChanged(System.Object sender, Xamarin.CommunityToolkit.UI.Views.TabSelectionChangedEventArgs e)
+        {
+            if (sender is TabView tabView)
+            {
+                var tabViewItem = tabView.TabItems[e.NewPosition];
+
+                if (tabViewItem.Content.BindingContext is IInitialize viewModel)
+                    await viewModel.InitializeAsync().ConfigureAwait(false);
+            }
         }
     }
 }
