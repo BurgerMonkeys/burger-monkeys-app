ce88ac36afd181e9ec817e5913c615ce718cc9e4
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-08 22:40:52 -0300

    Implementar carregamento da primeira aba ao abrir tela

--- BurgerMonkeys/BurgerMonkeys/Views/MainPage.xaml.cs
+++ BurgerMonkeys/BurgerMonkeys/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using BurgerMonkeys.Abstractions;
+﻿using System.Threading.Tasks;
+using BurgerMonkeys.Abstractions;
 using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 
@@ -11,15 +12,30 @@ namespace BurgerMonkeys
             InitializeComponent();
         }
 
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (Content is TabView tabView)
+            {
+                var tabViewItem = tabView.TabItems[0];
+                await InitializeAsync(tabViewItem).ConfigureAwait(false);
+            }
+        }
+
         async void TabView_SelectionChanged(System.Object sender, Xamarin.CommunityToolkit.UI.Views.TabSelectionChangedEventArgs e)
         {
             if (sender is TabView tabView)
             {
                 var tabViewItem = tabView.TabItems[e.NewPosition];
-
-                if (tabViewItem.Content.BindingContext is IInitialize viewModel)
-                    await viewModel.InitializeAsync().ConfigureAwait(false);
+                await InitializeAsync(tabViewItem).ConfigureAwait(false);
             }
+        }
+
+        private static async Task InitializeAsync(TabViewItem tabViewItem)
+        {
+            if (tabViewItem.Content.BindingContext is IInitialize viewModel)
+                await viewModel.InitializeAsync().ConfigureAwait(false);
         }
     }
 }
